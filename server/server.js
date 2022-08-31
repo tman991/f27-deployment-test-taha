0fb04a9cfc4cc520692d3250a8fc34a08f6c789c
Taha Manjoura <t.manjoura@gmail.com>
2022-08-31 13:57:44 -0500

    css added

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "../client/index.html"))
 })
 
+app.get('/css', () => {
+    res.sendFile(path.join(__dirname, "../client/styles.css"))
+})
 
 const port = process.env.PORT || 4005;
 
