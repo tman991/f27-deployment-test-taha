554ab7bf0696b0ca43c2f31c45752cb2d426109b
Taha Manjoura <t.manjoura@gmail.com>
2022-08-31 14:14:05 -0500

    js added

---
+++ client/client.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('button');
+
+const alertMe = () => {
+    alert('Hi there!');
+};
+
+btn.addEventListener('click', alertMe);
--- server/server.js
+++ server/server.js
@@ -12,6 +12,11 @@ app.get('/css', (req, res) => {
     res.sendFile(path.join(__dirname, "../client/styles.css"))
 })
 
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/client.js"))
+})
+
+
 const port = process.env.PORT || 4005;
 
 app.listen(port, () => {
